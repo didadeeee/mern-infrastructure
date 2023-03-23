05d7453bc9f1e8caa3f5a379a327ce4d1aed48ad
Ida Tang <idatangxinen@gmail.com>
2023-03-23 11:49:33 +0800

    setup server and proxy

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("/api", (req, res) => {
+  res.json({ msg: "Hi" });
+});
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
